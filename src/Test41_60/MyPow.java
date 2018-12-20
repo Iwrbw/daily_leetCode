e2309463815b8df08cf90a887ef4c7baeb5d98bb
yangshunfan <15700734908@163.com>
2018-12-20 22:26:11 +0800

    pow()

---
+++ src/Test41_60/MyPow.java
@@ -0,0 +1,12 @@
+package Test41_60;
+
+/**
+ * @author yangshunfan 2018/12/20 21:41
+ * 50. Pow(x, n)
+ */
+public class MyPow {
+    public double myPow(double x, int n) {
+
+        return Math.pow(x, n);
+    }
+}
